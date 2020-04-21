e0ea6946d713540239210b11c51474cb5c30e511
Sayan Bhattacharyya <sohamb03@outlook.com>
2020-04-21 16:48:12 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'dego',         
+  packages = ['dego'],
+  version = '0.0.1',      
+  license='MIT',
+  description = 'The Python RPC Wrapper for DeroGoldd and WalletAPI.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',   
+  author = 'Sayan Bhattacharyya',                
+  author_email = 'sohamb03@outlook.com',    
+  url = 'https://github.com/sohamb03/dego-py', 
+  download_url = 'https://github.com/sohamb03/dego-py/archive/v_001.tar.gz',
+  python_requires=">=3.6",
+  install_requires=[           
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+  project_urls={
+        'Documentation': 'https://dego-py.sohamb03.me',
+  },
+)
